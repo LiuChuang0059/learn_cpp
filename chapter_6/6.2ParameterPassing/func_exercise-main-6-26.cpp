d3103525347078ece7ade5807f41bb3004a0c2d5
刘闯WHU <1659608216@qq.com>
2018-08-24 18:14:39 +0800

    Add files via upload

---
+++ chapter_6/6.2ParameterPassing/func_exercise-main-6-26.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+
+
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+	std::string str;
+	for (int i = 1; i != argc; ++i)
+		str += std::string(argv[i]) + " ";
+
+	std::cout << str << std::endl;
+	return 0;
+}
+
